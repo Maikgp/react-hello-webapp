eb826579a9f03d09859bb1b775e345749cb8e551
Maikgp <154547587+Maikgp@users.noreply.github.com>
2024-03-25 21:41:06 +0100

    preparando

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,44 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			contacts: [],
+
 		},
 		actions: {
+			createContact: (fullName, email, address, phoneNumber) => {
+				fetch('https://playground.4geeks.com/apis/fake/contact/', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({
+						"full_name": fullName,
+						"email": email,
+						"agenda_slug": "Maikgp",
+						"address": address,
+						"phone": phoneNumber
+
+					})
+				})
+					.then(Response => Response.json())
+					.then(data => console.log(data))
+					.catch(error => console.log('Error;', error));
+			},
+			getContacts: () => {
+				fetch("https://playground.4geeks.com/apis/fake/contact/agenda/Maikgp")
+					.then(Response => Response.json())
+					.then(data => {
+						setStore({ contacts: data})
+						console.log(data)
+					})
+					.catch(error => console.log('Error;', error));
+
+			},
+			/* createContact: () => {
+
+			}  */
 			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
 		}
 	};
 };
--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -7,36 +7,78 @@ import "../../styles/demo.css";
 
 export const Demo = () => {
 	const { store, actions } = useContext(Context);
+	const [fullname, setFullname] = useState("");
+	const [email, setEmail] = useState("");
+	const [phonenumber, setPhoneNumber] = useState("");
+	const [address, setAddress] = useState("");
+	const handleSubmit = e => {
+		e.preventDefault();
+		actions.createContact(fullname, email, phonenumber, address);
+		setFullname("");
+		setEmail("");
+		setPhoneNumber("");
+		setAddress("");
+	}
 
 	return (
 		<div className="container">
-			<ul className="list-group">
-				{store.demo.map((item, index) => {
-					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
-								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
-					);
-				})}
-			</ul>
-			<br />
+			<form onSubmit={handleSubmit}>
+				<div className="mb-3">
+					<label htmlFor="exampleInputEmail1" className="form-label">Full name</label>
+					<input
+						type="text"
+						className="form-control"
+						id="exampleInputEmail1"
+						aria-describedby="emailHelp"
+						value={fullname}
+						onChange={e => setFullname(e.target.value)}
+						placeholder="Maikel Guerra"
+					/>
+
+					<div className="mb-3">
+						<label htmlFor="exampleInputPassword1" className="form-label">Email</label>
+						<input
+							type="email"
+							className="form-control"
+							id="exampleInputPassword1"
+							value={email}
+							onChange={e => setEmail(e.target.value)}
+							placeholder="GuerraM@"
+						/>
+					</div>
+					<div className="mb-3">
+						<label htmlFor="exampleInputPassword1" className="form-label">Phone number</label>
+						<input
+							type="text"
+							className="form-control"
+							id="exampleInputPassword1"
+							value={phonenumber}
+							onChange={e => setPhoneNumber(e.target.value)}
+							placeholder="+34000000"
+						/>
+					</div>
+					<div className="mb-3">
+						<label htmlFor="exampleInputPassword1" className="form-label">Address</label>
+						<input
+							type="text"
+							className="form-control"
+							id="exampleInputPassword1"
+							value={address}
+							onChange={e => setAddress(e.target.value)}
+							placeholder="marmolina 22"
+						/>
+					</div>
+				</div>
+
+
+				<button type="submit" className="btn btn-primary">Submit</button>
+			</form>
+
+			<button onClick={() => console.log(fullname)}>Mostrar datos</button>
+
+
 			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
+				<button>Back home</button>
 			</Link>
 		</div>
 	);
